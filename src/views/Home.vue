7a186d3a1dec340e59701dede50cf5c56f622cf8
Hector Zhao <hector.zhao@fluxfederation.com>
2019-05-01 12:14:24 +1200

    Add meal plans at the bottom of the landing page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,18 +2,21 @@
   <div>
     <home-landing></home-landing>
     <home-details></home-details>
+    <home-plans></home-plans>
   </div>
 </template>
 
 <script>
 import HomeLanding from '../components/HomeLanding';
 import HomeDetails from '../components/HomeDetails';
+import HomePlans from '../components/HomePlans';
 
 export default {
   name: 'home',
   components: {
     HomeLanding,
-    HomeDetails
+    HomeDetails,
+    HomePlans,
   }
 };
 </script>
